f5bb4d53568878dfc48ec269b6a04b1c6d9d92f0
Benjamin Rodriguez <benjamin_rodriguez@epam.com>
2026-02-22 14:40:21 -0600

    Useful commands for strings

---
+++ Section1/script3.py
@@ -0,0 +1,6 @@
+texto = "We are learning/ Python for DevOps"
+#Separated by space use split with double comillas ""
+new_texto = texto.split("/")
+print("Words:",new_texto)
+
+print(texto.split("/")[1])
