12bcf07568a2bf17581c4a9f98bfbbb8c688eddb
Benjamin Rodriguez <benjamin_rodriguez@epam.com>
2026-02-22 14:33:02 -0600

    Strings basic

--- Section1/script2.py
+++ Section1/script2.py
@@ -1,2 +1,12 @@
-texto = "Python is very demanding"
+texto = "Python is Very demanding"
 print(texto)
+largo = len(texto)
+print("Length of the string is: ",largo)
+upper = texto.upper()
+print("text in caps:",upper)
+lower = texto.lower()
+print("text in low:",lower) 
+
+#change Python to another word (word to find, word to change into)
+new_texto= texto.replace("Python","Linux")
+print("Nuevo texto:",new_texto)
