238c2327d391029bc7b34771f1ecc381ef687ec3
zenoheld1994 <luis.ben.94@gmail.com>
2026-03-25 19:50:28 +0100

    some stuff we did

---
+++ script7.py
@@ -0,0 +1,17 @@
+#Boolean
+# Only True and False 
+#Evaluae conditions, control the flow and handle logical operations
+#Result of comparison operatos like =, !=, >
+#Built in
+#Used in conditionals
+is_server_up= False
+if is_server_up:
+    print("Running!")
+else:
+    print("Ya valio barriga senor verga")
+a = 20
+b = 20
+if a==b:
+    print("igualitos")
+else:
+    print("ni madres")
