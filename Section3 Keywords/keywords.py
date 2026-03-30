e16d27fd2389d04d359f7d0849f4cdbffe0afe12
zenoheld1994 <luis.ben.94@gmail.com>
2026-03-30 22:45:48 +0200

    keywords of Python

---
+++ Section3 Keywords/keywords.py
@@ -0,0 +1,34 @@
+#for any programming language there are 4 pillars, if you master them you can learn any programming language
+# Keywords
+# Data Types
+# operators
+# Logical, reasoning and skills
+
+#Keywords have a meaning on the language
+#Keywords cannot be used as variable names, function names or identifiers, only to defien structute and logic
+#Keywords are case sensitive in python
+#Reserved, case - sensitive, fixed Set (predefined number of keywords)
+#Control flow
+# if /elif /else 
+#for /while
+#yield
+#Global non local
+#import from as 
+#OOP self is, in not in
+#Misc assert del with
+import keyword
+#print(keyword.kwlist)
+#Starts at 0
+# for i in range(10):
+#    if i ==4 :
+#       break
+#    print(i)
+# i = 0
+# while i <3:
+#     print(i)
+#     i+=1
+# Continue used to skip the loop
+for i in range(7):
+   if i ==4 :
+      continue
+   print(i)
