1430c44e29f48a598b591f614d20f87815e81958
zenoheld1994 <luis.ben.94@gmail.com>
2026-03-30 21:23:09 +0200

    replace stirng swith regex

--- Section2 Regex/regularex1.py
+++ Section2 Regex/regularex1.py
@@ -1,5 +1,14 @@
-import region 
+import re
 text = "Hello how are you \
     My name is Benjamin Rodriguez \
         I love python \
-            I hope you enjoy everything else with me !!!!"
+            I hope you enjoy learning python and everything else with me !!!!"
+#pattern = r"python"
+#re.findall finds all the occurences within the string and returns those words
+#result = re.findall(pattern,text)
+#print(result)
+#re.sub replaces the matches with a given string useful for scripts that need to be changed
+text0= "Python    is    amazing and thats it ttttttt"
+pattern = r"t+t"
+result = re.sub(pattern,"puto" ,text0)
+print(result)
