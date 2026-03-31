07d64be3cc80eb60813a8e7a158494c9189c5398
zenoheld1994 <luis.ben.94@gmail.com>
2026-03-31 15:49:19 +0200

    basics of functions

--- Section4 Variables/functionreturns1.py
+++ Section4 Variables/functionreturns1.py
@@ -1,4 +1,6 @@
-#Return exit a function and send back a value to the claler.
+#Starting here i am using functions to stop the functionality of the code, to check something just uncomment the functions with 
+#names like first, second, etc...
+#Return exit a function and send back a value to the caller.
 #When return is found inside a function the function stops and returns the value to the caller
 #if no value is specified the function returns None
 #We always use a return inside a function
@@ -22,3 +24,20 @@ def second():
     print(details)
 
 # second()
+def check_even(number):
+    if number % 2 == 0:
+        return "Even"
+    return "Odd"
+def third():
+    result = check_even(8)
+    print(result)
+# third()
+
+def maximum_value(numbl):
+    return max(numbl)
+
+list_num=[1,2,5,9,0,10]
+result = maximum_value(list_num)
+print(result)
+result = result**2
+print(result)
